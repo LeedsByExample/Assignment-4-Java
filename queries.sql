b03cd5791a01901b5f7138033a08bc59f4449e83
LeedsByExample <leeda.minkin@yahoo.com>
2024-02-14 18:51:44 -0600

    task-1 complete

--- queries.sql
+++ queries.sql
@@ -1,5 +1,8 @@
---Part 1
-
+--Part 1 (list the columns and their data types in the table as a SQL comment.)
+    --id: int
+    --employer: varchar(255)
+    --name: varchar(255)
+    --skills: varchar(255)
 --Part 2
 
 --Part 3
